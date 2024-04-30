9792202ede85f6ba4a3f768675ae8b22d64fa3e0
John <mjohncorbet@gmail.com>
2024-04-30 21:26:01 +0800

    setup sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,11 @@ export default defineConfig({
         usePolling: true
     }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+},
 })
