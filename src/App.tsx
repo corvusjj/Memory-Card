01a474b28a3d2b1192cd5256a21b5b4d82407c93
John <mjohncorbet@gmail.com>
2024-05-05 20:48:48 +0800

    get pokemon data with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,44 @@ async function fetchPokemonData(link:string) {
         }
 
         const pokemonData = await response.json();
-        console.log(pokemonData);
         return pokemonData;
 
     } catch(error) {
-        console.error(error);
-        throw error;
+        if (error instanceof Error) {
+            console.log(error.message, link);
+            throw error;
+        }
     }
 }
 
 async function getPokemonDataSet(idSet:number[]) {
     let errorOccurred = false;
+    const pokemonDataSet = [];
 
     for(let i=0; i < idSet.length; i++) {
         try {
             if (errorOccurred) break;
             const link = `https://pokeapi.co/api/v2/pokemon/${idSet[i]}/`;
-            await fetchPokemonData(link);
+            const pokemonData = await fetchPokemonData(link);
+            
+            console.log(pokemonData.name);
+            pokemonDataSet.push(pokemonData);
         } catch(error) {
             if (error instanceof Error) {
-                console.log(error.message);
+                console.log(error.message, `index:${i} id:${idSet[i]}`);
                 errorOccurred = true;
+                throw error;
             }
         }
     }
+
+    return pokemonDataSet;
 }
 
-function generateRandomIds() {
+function generateRandomIds(num: number) {
     const randomIds:number[] = [];
 
-    for (let i=0; i < 12; i++) {
+    for (let i=0; i < num; i++) {
         const randomNum = Math.floor(Math.random() * 600);
         randomIds.push(randomNum);
     }
@@ -49,8 +57,23 @@ function generateRandomIds() {
 
 function App() {
     useEffect(() => {
-        const idSet = generateRandomIds();
-        getPokemonDataSet(idSet);
+        let ignore = false;
+
+        async function getData() {
+            if (!ignore) {
+                const idSet = generateRandomIds(12);
+
+                await getPokemonDataSet(idSet)
+                .then((data) => console.log(data))
+                .catch((error) => console.log(error))
+            }
+        }
+
+        getData();
+
+        return () => {
+            ignore = true;
+        }
     }, []);
 
     return (
