a984542edb5e318ff201c0c93052d544a87f0f53
John <mjohncorbet@gmail.com>
2024-05-09 22:40:28 +0800

    setup pokemon cries audio

--- src/components/PokemonCell.tsx
+++ src/components/PokemonCell.tsx
@@ -1,15 +1,26 @@
+import { useRef } from 'react';
 import { RawData } from '../types/pokemon';
- 
+
 interface PokemonDataProps {
     pokemonData: RawData
 }
 
 export default function PokemonCell({pokemonData}: PokemonDataProps) {
-    // console.log(pokemonData);
+    const audioRef = useRef<HTMLAudioElement>(null)
+
+    function playCryAudio() {
+        if (audioRef.current) {
+            audioRef.current.volume = 0.5;
+            audioRef.current.play();
+        }
+    }
 
     return (
         <div className='pokemon-cell'>
             <img src={pokemonData.sprite} alt="pokemon" />
+            <button onClick={playCryAudio}>Cry</button>
+
+            <audio ref={audioRef} src={pokemonData.cryAudio}></audio>
         </div>
     );
 }
