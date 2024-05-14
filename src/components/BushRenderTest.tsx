fccc3ec1464f98ea44b290f4ebb8503f48ca4bfb
John <mjohncorbet@gmail.com>
2024-05-14 16:35:09 +0800

    setup bush animation for shuffling pokemons

--- src/components/BushRenderTest.tsx
+++ src/components/BushRenderTest.tsx
@@ -1,21 +1,36 @@
 import { useRef } from "react";
 
 export default function BushRenderTest() {
-    const spriteRef = useRef<HTMLImageElement>(null);
+    const bushSpritesRef = useRef<HTMLDivElement>(null);
 
-    function shakeBush() {
-        spriteRef.current?.classList.add('shake');
+    function delay(ms:number) {
+        return new Promise(res => setTimeout(res, ms));
+    }
 
-        setTimeout(() => {
-            spriteRef.current?.classList.remove('shake');
-        }, 700);
+    async function animateBush() {
+        if (bushSpritesRef.current) {
+            const bushSprites: HTMLImageElement[] = Array.from(bushSpritesRef.current.childNodes) as HTMLImageElement[];
+
+            let lastBushSprite;
+            let activeBushSprite = bushSprites[2];
+            const indexPattern = [1, 0, 1, 2];
+
+            for(let i=0; i<indexPattern.length; i++) {
+                lastBushSprite = activeBushSprite;
+                activeBushSprite = bushSprites[indexPattern[i]];
+                lastBushSprite.classList.remove('show');
+                activeBushSprite.classList.add('show');
+
+                await delay(150);
+            }
+        }
     }
 
     return (
-        <div className="bush-render-test">
-            {/* <img src="../../images/bush-1.webp" alt="" /> */}
-            {/* <img src="../../images/bush-2.webp" alt="" /> */}
-            <img ref={spriteRef} onClick={shakeBush} src="../../images/bush-3.webp" alt="" />
+        <div ref={bushSpritesRef} onClick={animateBush} className="bush-render-test">
+            <img src="../../images/bush-1.webp" alt="" />
+            <img src="../../images/bush-2.webp" alt="" />
+            <img className="show" src="../../images/bush-3.webp" alt="" />
         </div>
     );
 }
