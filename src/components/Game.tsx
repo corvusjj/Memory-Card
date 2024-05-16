a330fc8c526a580343daa9ff85e2f8bfcfb3eb22
John <mjohncorbet@gmail.com>
2024-05-16 13:04:06 +0800

    modify state to game-data, pokemon-set and hitIds array added

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,37 +7,50 @@ interface RawDataProps {
     pokemonDataSet: RawData[];
 }
 
+interface GameDataProps {
+    pokemons: RawData[];
+    hitIds: number[];
+}
+
 export default function Game({pokemonDataSet}:RawDataProps) {
-    const [pokemons, setPokemons] = useState(pokemonDataSet);
-    const hitPokemonIds:number[] = [];
+    // const [pokemons, setPokemons] = useState(pokemonDataSet);
+
+    const [gameData, setGameData] = useState<GameDataProps>({
+        pokemons: pokemonDataSet,
+        hitIds: []
+    });
 
     const scoreBoardRef = useRef<ScoreBoardRef>(null);
 
     function shufflePokemons() {
-        setPokemons(pokemons => {
-            return [...pokemons.sort(() => Math.random() - 0.5)];
-        });
+        return [...gameData.pokemons.sort(() => Math.random() - 0.5)];
     }
 
-    function runHit(id:number) {
-        console.log(hitPokemonIds);
-        if (hitPokemonIds.includes(id)) {
-            console.log('game-over');
-        } else {
-            hitPokemonIds.push(id);
-
-            setTimeout(shufflePokemons, 2000);
-        }
+    function updateScoreBoard() {
         if (scoreBoardRef.current) {
             scoreBoardRef.current.addScore();
+        }
+    }   
+
+    function runHit(id:number) {
+        if (gameData.hitIds.includes(id)) {
+            console.log('game-over');
+        } else {
+            updateScoreBoard();
+
+            const newHitIds = [...gameData.hitIds, id];
+            const shuffledPokemonSet = shufflePokemons();
+            setGameData({pokemons: shuffledPokemonSet, hitIds: newHitIds});
         }
     }
 
     return (
         <div className="game">
-            <button onClick={shufflePokemons}>Shuffle</button>
+            {/* <button onClick={shufflePokemons}>Shuffle</button> */}
             <ScoreBoard ref={scoreBoardRef} />
-            <PokemonBoard pokemonRawData={pokemons} runHit={runHit}/>
+            <PokemonBoard pokemonRawData={gameData.pokemons} runHit={runHit}/>
         </div>
     );
 }
+
+//  updateScore() scoreBoard
