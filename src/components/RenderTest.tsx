211f39215250e303a25eeb08e35e58673512de89
John <mjohncorbet@gmail.com>
2024-05-18 03:15:24 +0800

    setup thrower-sprite animation

---
+++ src/components/RenderTest.tsx
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+
+export default function RenderTest() {
+    const throwerSprite = useRef<HTMLImageElement>(null);
+    const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
+
+    async function animateThrower() {
+        if (throwerSprite.current) {
+            let leftDistance = 0;
+
+            while (leftDistance <= 400) {
+                throwerSprite.current.style.left = '-' + leftDistance + '%';
+                leftDistance += 100;
+                await delay(150);
+            }
+
+            throwerSprite.current.style.left = '0%';
+        }
+    }
+
+    return (
+        <>
+            <button onClick={animateThrower}>Throw</button>
+            <div className="thrower-container">
+                <img ref={throwerSprite} src="../../images/thrower.png" alt="" />
+            </div>
+        </>
+    );
+}
