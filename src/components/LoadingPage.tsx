9964f1113162b9a5bcd5fb9a5dc17e41fe737b08
John <mjohncorbet@gmail.com>
2024-05-20 18:30:09 +0800

    add span / re-style loading page

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,7 +1,8 @@
 export default function LoadingPage() {
     return (
         <div id="loading-page">
-            {/* <img src="../images/oak-dance.gif" alt="" /> */}
+            <img src="../images/oak-dance.gif" alt="" />
+            <span>Smuggling pokemons...</span>
         </div>
     );
 }
